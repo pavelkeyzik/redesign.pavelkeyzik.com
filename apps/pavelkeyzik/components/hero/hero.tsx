1e78a7edd79794da458e4d0b1dd60f3d1850b032
Pavel Keyzik <pavel.keyzik@gmail.com>
2021-12-12 00:49:14 +0300

    feat: Add list of technologies in hero component

--- apps/pavelkeyzik/components/hero/hero.tsx
+++ apps/pavelkeyzik/components/hero/hero.tsx
@@ -1,13 +1,47 @@
 import { Button } from '@pavelkeyzik/design-system';
+import { ReactComponent as ReactIcon } from '../../assets/icons/react.svg';
+import { ReactComponent as CssIcon } from '../../assets/icons/css.svg';
+import { ReactComponent as FigmaIcon } from '../../assets/icons/figma.svg';
+import { ReactComponent as JavaScriptIcon } from '../../assets/icons/javascript.svg';
+import { ReactComponent as JestIcon } from '../../assets/icons/jest.svg';
+import { ReactComponent as NextIcon } from '../../assets/icons/next.svg';
+import { ReactComponent as SassIcon } from '../../assets/icons/sass.svg';
 import styles from './hero.module.scss';
 
 export function Hero() {
   return (
-    <header className={styles.heroContainer}>
-      <h1>Hi! My name&apos;s Pavel Keyzik</h1>
-      <p>I&apos;m a Front-end Engineer and I really love to work with React</p>
-      <Button className="mt-6">Wanna see my last works?</Button>
-    </header>
+    <div className={styles.heroMainContainer}>
+      <div className={styles.technologiesGrid}>
+        <div className={styles.technologiesGridItem}>
+          <ReactIcon />
+        </div>
+        <div className={styles.technologiesGridItem}>
+          <CssIcon />
+        </div>
+        <div className={styles.technologiesGridItem}>
+          <FigmaIcon />
+        </div>
+        <div className={styles.technologiesGridItem}>
+          <JavaScriptIcon />
+        </div>
+        <div className={styles.technologiesGridItem}>
+          <JestIcon />
+        </div>
+        <div className={styles.technologiesGridItem}>
+          <NextIcon />
+        </div>
+        <div className={styles.technologiesGridItem}>
+          <SassIcon />
+        </div>
+      </div>
+      <header className={styles.heroContainer}>
+        <h1>Hi! My name&apos;s Pavel Keyzik</h1>
+        <p>
+          I&apos;m a Front-end Engineer and I really love to work with React
+        </p>
+        <Button className="mt-6">Wanna see my last works?</Button>
+      </header>
+    </div>
   );
 }
 
